267a26ca0f41466b8695baa3aea8ca2bb2bff105
MaksimVolkov <skazimko@yandex.ru>
2024-12-25 22:57:02 +0300

    create forms.py

---
+++ mailing/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Recipient, Message, Mailing
+class RecipientForm(forms.ModelForm):
+    class Meta:
+        model = Recipient
+        fields = ["email", "full_name", "comment"]
+    def __init__(self, *args, **kwargs):
+        super(RecipientForm, self).__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({"class": "form-control"})
+        self.fields["full_name"].widget.attrs.update({"class": "form-control"})
+        self.fields["comment"].widget.attrs.update({"class": "form-control"})
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ["topic", "text"]
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+        self.fields["topic"].widget.attrs.update({"class": "form-control"})
+        self.fields["text"].widget.attrs.update({"class": "form-control"})
+class MailingForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        exclude = ("first_sending",)
+    def __init__(self, *args, **kwargs):
+        super(MailingForm, self).__init__(*args, **kwargs)
+        self.fields["end_sending"].widget.attrs.update({"class": "form-control"})
+        self.fields["status"].widget.attrs.update({"class": "form-control"})
+        self.fields["message"].widget.attrs.update({"class": "form-control"})
+        self.fields["recipients"].widget.attrs.update({"class": "form-control"})
