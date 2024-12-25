a121302bd346fd104cf2544270f318dbc9a35d1c
MaksimVolkov <skazimko@yandex.ru>
2024-12-25 22:53:35 +0300

    add CRUD

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from mailing.models import Recipient, Message, Mailing
+
+
+class RecipientListView(ListView):
+    model = Recipient
+
+
+class RecipientDetailsView(DetailView):
+    model = Recipient
+
+
+class RecipientCreateView(CreateView):
+    model = Recipient
+    form_class = RecipientForm
+    success_url = reverse_lazy("mailing:recipient_list")
+
+
+class RecipientUpdateView(UpdateView):
+    model = Recipient
+    form_class = RecipientForm
+    success_url = reverse_lazy("mailing:recipient_list")
+
+
+class RecipientDeleteView(DeleteView):
+    model = Recipient
+    success_url = reverse_lazy("mailing:recipient_list")
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailsView(DetailView):
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy("mailing:message_list")
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy("mailing:message_list")
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy("mailing:message_list")
+    class MailingListView(ListView):
+        model = Mailing
+    class MailingDetailsView(DetailView):
+        model = Mailing
+    class MailingCreateView(CreateView):
+        model = Mailing
+        form_class = MailingForm
+        success_url = reverse_lazy("mailing:mailing_list")
+    class MailingUpdateView(UpdateView):
+        model = Mailing
+        form_class = MailingForm
+        success_url = reverse_lazy("mailing:mailing_list")
+    class MailingDeleteView(DeleteView):
+        model = Mailing
+        success_url = reverse_lazy("mailing:mailing_list")
