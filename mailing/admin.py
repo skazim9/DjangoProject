578a543f7e0e3cfbb8801d7a2726739acd106d28
MaksimVolkov <skazimko@yandex.ru>
2024-12-26 10:29:16 +0300

    update for message

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from mailing.models import Recipient
+from mailing.models import Recipient, Message
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
     list_display = ("full_name", "email")
     search_fields = ('full_name', 'email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("topic",)
+    ordering = ("topic",)
+    search_fields = ("topic", "text")
--- mailing/urls.py
+++ mailing/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 
 from mailing.apps import MailingConfig
 from mailing.views import RecipientListView, RecipientDetailsView, RecipientCreateView, RecipientUpdateView, \
-    RecipientDeleteView
+    RecipientDeleteView, MessageDetailsView, MessageCreateView, MessageUpdateView, MessageDeleteView, MessageListView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', RecipientListView.as_view(), name='recipient_list'),
+    path('a', RecipientListView.as_view(), name='recipient_list'),
     path('recipient/<int:pk>/', RecipientDetailsView.as_view(), name='recipient'),
-    path("new/", RecipientCreateView.as_view(), name="recipient_create"),
-    path("update/<int:pk>/", RecipientUpdateView.as_view(), name="recipient_update"),
-    path("delete/<int:pk>/", RecipientDeleteView.as_view(), name="recipient_delete"),
+    path("new/recipient/", RecipientCreateView.as_view(), name="recipient_create"),
+    path("update/recipient/<int:pk>/", RecipientUpdateView.as_view(), name="recipient_update"),
+    path("delete/recipient/<int:pk>/", RecipientDeleteView.as_view(), name="recipient_delete"),
+    path('', MessageListView.as_view(), name='message_list'),
+    path('message/<int:pk>/', MessageDetailsView.as_view(), name='message'),
+    path("new/message/", MessageCreateView.as_view(), name="message_create"),
+    path("update/message/<int:pk>/", MessageUpdateView.as_view(), name="message_update"),
+    path("delete/message/<int:pk>/", MessageDeleteView.as_view(), name="message_delete"),
 ]
